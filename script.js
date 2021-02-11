2865201c94a02668e67ffff50dc30648bdb265cc
YAGoOaR <yegorgribenko68@gmail.com>
2021-02-11 19:09:34 +0200

    Added file loading

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
 
 const LABS_DONE = 5;
-const labsArr = ["lab1.html"];
 
 const buttonDiv = document.getElementById('buttonDiv1');
+const labDiv = document.getElementById('labDiv');
 
-for(let i = 0; i < LABS_DONE; i++){
-  let button = document.createElement('button');
-  button.class = "button-1";
-  button.innerHTML = "Лабораторна робота " + (i + 1);
+let currentLab = null;
+
+function buttonClicked(labId) {
+  if(currentLab){
+    currentLab.remove();
+  }
+  const lab = document.createElement('iframe');
+  lab.src = `Lab${labId}.html`
+  lab.style.width = "100%";
+  currentLab = lab;
+  labDiv.append(lab);
+}
+
+for(let i = 1; i <= LABS_DONE; i++){
+  const button = document.createElement('button');
+  button.className = "button-1";
+  button.innerHTML = "Лабораторна робота " + i;
+  button.onclick = buttonClicked.bind(null, i);
   buttonDiv.append(button);
 }
